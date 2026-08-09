5eaecd271b0368bbbf8f92ae06fc906242cc65c9
accthanhtai-ui <accthanhtai@gmail.com>
2026-08-10 00:59:40 +0700

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class BookCreate(BaseModel):
+    title : str
+    author : str
+    category : str
+    price : float
+    borrow_count : int = 0
+    available_quantity : int = 0
